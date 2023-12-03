a606f919903e001f664f16d1d71a070198cdf3ce
Tiffany Alcorn <alcorn.tiff@gmail.com>
2023-12-03 13:40:20 -0500

    figured out why the padding wasnt going away

--- vue/src/components/PhotoCard.vue
+++ vue/src/components/PhotoCard.vue
@@ -19,12 +19,21 @@ article {
     background-color: #9eb8d9;
     flex-direction: column;
     align-items: center;
-    padding: 10px, 0px, 10px, 0px;
+    padding: 10px 0px 10px 0px;
+    margin: 0px;
+    width: 1000px;
+}
 
-
+h2{
+    background-color: black;
+    margin: 0px;
+    padding: 0px;
 }
 
 img {
     height: 150px;
+    margin: 0px;
+    width: auto;
+    
 }
 </style>
