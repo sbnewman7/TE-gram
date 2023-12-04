a15d165330ceaa987d633d6d8ac8eb25d0ab0964
Tiffany Alcorn <alcorn.tiff@gmail.com>
2023-12-04 14:23:38 -0500

    started user profile

---
+++ vue/src/views/UserView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>User Profile</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+h1{
+  font-size: 3.5rem;
+  color: rgb(55, 55, 157);
+margin-top: 13px;
+margin-bottom: 13px;
+display: flex;
+justify-content: center;
+}
+</style>
