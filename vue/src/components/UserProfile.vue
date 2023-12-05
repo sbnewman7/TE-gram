51c8d2f6b5057036f1ee929d5548e830d685d98d
Russell Hoffman <russellhoffman@optonline.net>
2023-12-05 09:51:21 -0500

    make user profile pic a circle

--- vue/src/components/UserProfile.vue
+++ vue/src/components/UserProfile.vue
@@ -1,41 +1,61 @@
 <template>
-    <div>
-      <span>Profile Pic: </span> <img :src="pictureUrl">
-      <cloudinary-upload v-model:pictureUrl="pictureUrl"/>
-      <p>User Name: {{ user.username }}</p>
-      <p>User Email: {{ user.email }}</p>
-      <p>User Password: *********</p>
-      
+  <div>
+    <span>Profile Pic: </span>
+    <div class="circular--landscape">
+      <img v-if="pictureUrl" :src="pictureUrl">
     </div>
-  </template>
+    <div class="circular--landscape">
+      <img v-if="!pictureUrl" :src="user.picUrl">
+    </div>
+
+    <cloudinary-upload v-model:pictureUrl="pictureUrl" />
+    <p>User Name: {{ user.username }}</p>
+    <p>User Email: {{ user.email }}</p>
+    <p>User Password: *********</p>
+
+  </div>
+</template>
   
-  <script>
-    
+<script>
+
 import CloudinaryUpload from '../components/CloudinaryUpload.vue';
 
 
-  export default {
-    components: {
+export default {
+  components: {
     CloudinaryUpload
   },
-    props: ['user'],
-    created() {
+  props: ['user'],
+  created() {
     console.log('User object:', this.user);
   },
   methods: {
-    changePic(){
-        this.$store.state.pictureUrl;
+    changePic() {
+      this.$store.state.pictureUrl;
     }
   },
-  computed:{
-    pictureUrl(){
-        return this.$store.state.pictureUrl;
+  computed: {
+    pictureUrl() {
+      return this.$store.state.pictureUrl;
     }
   }
-    }
-  
-  </script>
+}
+
+</script>
 
 <style scoped>
+.circular--landscape {
+  display: inline-block;
+  position: relative;
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  border-radius: 50%;
+}
 
+.circular--landscape img {
+  width: auto;
+  height: 100%;
+  margin-left: -50px;
+}
 </style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -54,7 +54,7 @@ const routes = [
     name: 'user',
     component: UserView,
     meta: {
-      requiresAuth: false
+      requiresAuth: true
     }
   }
 ];
@@ -75,7 +75,7 @@ router.beforeEach((to) => {
 
   // If it does and they are not logged in, send the user to "/login"
   if (requiresAuth && store.state.token === '') {
-    return {name: "login"};
+    return { name: "login" };
   }
   // Otherwise, do nothing and they'll go to their next destination
 });
