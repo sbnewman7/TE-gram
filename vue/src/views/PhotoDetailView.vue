2e74a061e59ecb2088dcc83d2d1463d58ad8a06b
sarah newman <sbnewman7@gmail.com>
2023-12-07 18:35:44 -0500

    added like functionality to photo detail view

--- vue/src/views/PhotoDetailView.vue
+++ vue/src/views/PhotoDetailView.vue
@@ -4,7 +4,16 @@
       <div class="photo-container">
         <img class="photo" :src="photo.photoUrl" />
       </div>
-      <h1>{{ photo.caption }}</h1>
+
+      <div class="like-caption">
+        <img v-if="liked" v-on:click="unlike" class="heart"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/35/Red-simple-heart-symbol-only.png" alt="">
+        <img v-if="!liked" v-on:click="like" class="heart-dark"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/35/Red-simple-heart-symbol-only.png" alt="">
+        <p class="likeCount">{{ likeCount }}</p>
+        <h1>{{ photo.caption }}</h1>
+      </div>
+
     </section>
     <section id="left-half">
       <div id="comment" v-for="comment in photo.comments" :key="comment.id">
@@ -19,49 +28,85 @@
 <script>
 import PhotosGateway from "../services/PhotosGateway";
 import UserGateway from "../services/UserGateway";
+import LikesGateway from '../services/LikesGateway';
 
 export default {
   data() {
     return {
-      photo: {},
+      photo: {
+        id: 0
+      },
+
+      liked: false,
+      likeCount: 0,
+
       comment: {},
-      user: ""
+      commentUser: ""
     };
   },
   methods: {
+    like() {
+      if (this.$store.state.token !== '') {
+        this.liked = true;
+        this.likeCount++;
+        LikesGateway.addLike(this.photo.id, this.$store.state.user.id)
+      }
+    },
+    unlike() {
+      if (this.$store.state.token !== '') {
+        this.liked = false;
+        this.likeCount--;
+        LikesGateway.removeLike(this.photo.id, this.$store.state.user.id)
+      }
+    },
+
     getUserById(userId) {
       UserGateway
         .getUserById(userId)
         .then((response) => {
-          this.user = response.data.username;
+          this.commentUser = response.data.username;
         })
-      return this.user;
+      return this.commentUser;
     },
 
     formatDateTime(dateTimeString) {
       const date = new Date(dateTimeString);
-
       const month = (date.getMonth() + 1).toString().padStart(2, '0');
       const day = date.getDate().toString().padStart(2, '0');
       const year = date.getFullYear().toString();
-
       let hours = date.getHours();
       const ampm = hours >= 12 ? 'pm' : 'am';
       hours = hours % 12;
       hours = hours ? hours : 12; // Convert 0 to 12
       const minutes = date.getMinutes().toString().padStart(2, '0');
-
       const formattedDateTime = `${month}-${day}-${year} ${hours}:${minutes} ${ampm}`;
       return formattedDateTime;
     }
 
   },
   created() {
+    this.photo.id = this.$route.params.id;
     PhotosGateway
-      .getPhotoByPhotoId(this.$route.params.id)
+      .getPhotoByPhotoId(this.photo.id)
       .then((response) => {
         this.photo = response.data;
       });
+
+    if (this.$store.state.token !== '') {
+      LikesGateway
+        .getLiked(this.$store.state.user.id, this.photo.id)
+        .then((response) => {
+          if (response.data)
+            this.liked = true;
+        });
+      LikesGateway
+        .getLikeCount(this.photo.id)
+        .then((response) => {
+          if (response.data)
+            this.likeCount = response.data;
+        })
+    }
+
   },
 };
 </script>
@@ -108,5 +153,37 @@ section {
   height: 100%;
   object-fit: cover;
   overflow: hidden;
+}
+
+.like-caption {
+  display: flex;
+  font-weight: bold;
+  /* justify-content: star; */
+}
+
+.like-caption>h1 {
+  font-size: 25px;
+  margin-left: 12px;
+}
+
+.heart {
+  max-width: 50px;
+  border: none;
+}
+
+.heart-dark {
+  max-width: 50px;
+  opacity: 30%;
+  border: none;
+}
+
+.likeCount {
+  padding-top: 5px;
+  align-content: center;
+}
+
+.like-caption {
+  display: flex;
+  align-items: center;
 }
 </style>
