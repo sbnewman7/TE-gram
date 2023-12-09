f1c5224f1c40111cc84d7fd5bc397d5eccdad076
matt brinkman <mattcbrinkman@gmail.com>
2023-12-09 14:47:55 -0500

    follower backend commit

--- java/src/main/java/com/techelevator/dao/JdbcFollowDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcFollowDao.java
@@ -2,6 +2,7 @@ package com.techelevator.dao;
 
 
 import com.techelevator.exception.DaoException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -43,15 +44,43 @@ public class JdbcFollowDao implements FollowDao {
         final String sql = "SELECT COUNT(follower_user_id) FROM followers WHERE follower_user_id = ? AND followed_user_id = ?;";
         int numOfRows;
         try {
-            numOfRows = this.jdbcTemplate.queryForObject(sql, int.class, photoId, userid);
+            numOfRows = this.jdbcTemplate.queryForObject(sql, int.class, followerUserId, followedUserId);
 
         } catch (CannotGetJdbcConnectionException e) {
             throw new DaoException("Unable to connect to server or database", e);
         }
 
-        return numOfRows != 0;
+        return numOfRows > 0;
     }
 
+    @Override
+    public void addFollower(int followerUserId, int followedUserId) {
+        final String sql = "INSERT INTO followers(follower_user_id, followed_user_id) " +
+                "VALUES (?,?);";
+        try {
+            this.jdbcTemplate.update(sql, followerUserId, followedUserId);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation");
+        }
+    }
+
+    @Override
+    public void removeFollower(int followerUserId, int followedUserId) {
+        final String sql = "DELETE FROM followers " +
+                "WHERE follower_user_id = ? AND followed_user_id = ?;";
+        try {
+            this.jdbcTemplate.update(sql, followerUserId, followedUserId);
+
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation");
+        }
+    }
 }
+
+
 
 
